1e7c06e51fee36eb3be72eb79470431b68639a0a
kurniawan <kurni.aawann19@gmail.com>
2025-02-08 16:19:06 +0700

    Study Component

---
+++ src/Hello-world/HelloWorld.jsx
@@ -0,0 +1,21 @@
+ function HeaderhelloWorld(){
+    return (
+        <h1>Helo World</h1>
+    )
+}
+
+ function ParagraphHelloWorld(){
+    return (
+        <p>Belajar React Cuyy uhuy</p>
+    )
+}
+
+
+export function HelloWorld(){
+    return (
+        <>
+            <HeaderhelloWorld />
+            <ParagraphHelloWorld />
+        </>
+    )
+}
--- src/HelloWorld/HelloWord.jsx
+++
@@ -1,6 +0,0 @@
-export default function helloWorld(){
-    <div>
-        <h1>hello World</h1>
-        <p>Belajar React Dasar</p>
-    </div>
-}
